65905962f82d8724e1c817f4dc4a4bba9be66d5e
k1729326490 <118305262+k1729326490@users.noreply.github.com>
2024-03-01 14:20:14 +0800

    fork

--- RapidTestOrder.js
+++ RapidTestOrder.js
@@ -10,17 +10,14 @@ class RapidTestOrder {
       },
       RESERVING: (sInput) => {
         let aReturn = [];
-        this.isDone = true;
         if (sInput.toLowerCase().startsWith('y')) {
           this.stateCur = this.OrderState.ADDING_TOPPING; 
-          aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
-          let d = new Date();
-          d.setMinutes(d.getMinutes() + 120);
-          aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
+          aReturn.push(`Thank you for choosing Hi Pizza!`);
           aReturn.push("Would you like to add toppings to your pizza?"); 
         } else {
-          aReturn.push("Thanks for trying our reservation system");
+          aReturn.push("Thank you for considering Hi Pizza!");
           aReturn.push("Maybe next time");
+          this.isDone = true; // jiehsu
         }
         return aReturn;
       },
@@ -32,17 +29,23 @@ class RapidTestOrder {
         } else {
           this.stateCur = this.OrderState.RESERVING; 
           aReturn.push("No problem. Your order is being processed.");
+          this.isDone = true; // jiehsu
         }
         return aReturn;
       },
       ADDING_DRINK: (sInput) => {
         let aReturn = [];
-        this.isDone = true;
         if (sInput.trim() !== "") {
           aReturn.push(`You've added ${sInput} to your order.`);
         } else {
           aReturn.push("You haven't added any drink to your order.");
         }
+        aReturn.push("Thank you for your order!");
+        aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
+        let d = new Date();
+        d.setMinutes(d.getMinutes() + 120);
+        aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
+        this.isDone = true; // jieshu
         return aReturn;
       }
     };
@@ -51,13 +54,14 @@ class RapidTestOrder {
     this.isDone = false;
     this.sFrom = sFrom;
   }
+
   handleInput(sInput) {
     return this.stateCur(sInput);
   }
+
   isDone() {
     return this.isDone;
   }
 }
-
 
 export { RapidTestOrder }
